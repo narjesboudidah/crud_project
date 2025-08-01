9c848ae2332a1d35b18d19dfe6796acf0080bc9a
NarjesBoudidah <narjes.boudidah.73@gmail.com>
2025-08-01 19:43:30 +0100

    fix:erros

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"encoding/json"
 	"math/rand"
 	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 type Movie struct {
-	ID       string `json:"id"`
-	Isbn     string `json:"isbn"`
-	Title    string `json:"title"`
-	Director string `json:"director"`
+	ID       string   `json:"id"`
+	Isbn     string   `json:"isbn"`
+	Title    string   `json:"title"`
+	Director Director `json:"director"`
 }
+
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
@@ -23,90 +25,87 @@ type Director struct {
 
 var movies []Movie
 
-
-func getMovies(w http.Responsewriter , r *http.Request) 
-{
-	w.Header().Set("Content-Type","application/json")
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
 
-func deleteMovie( w http.Responsewriter , r *http.Request)
-{
-	w.Header().Set("Content-Type","application/json")
-	params:=mux.Vars(r)
-	for index,item:=range movies {
-		if item.ID==params["id"]{
-			movies=append(movies[:index],movies[index+1:]...)
-			break 
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
 		}
 	}
 	json.NewEncoder(w).Encode(movies)
-
 }
 
-func getMovie(w http.Responsewriter, r *http.Request)
-{
-	w.Header().Set("Content-Type","application/json")
-	params:=mux.Vars(r) 
-	for _,item:=range movies {
-		if item.ID==params["id"]{
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
-			return 
+			return
 		}
 	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Movie not found"))
 }
-func createMovie(w http.Responsewriter, r *http.Request)
-{
-	w.Header().Set("Content-Type","application/json")
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
-	_=json.NewEncoder(r,Body).Decode(&movie)
-	movie.ID=strconv.Itoa(rand.Intn(100000000))
-	movies=append(movies,movie)
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid request body"))
+		return
+	}
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
 
-func updateMovie(w http.Responsewriter, r *http.Request)
-{
-	w.Header().Set("Content-Type","application/json")
-	params:=mux.Vars(r) 
-	for index,item:=range movies {
-	if item.ID==params["id"]{
-		movies=append(movies[:index],movies[index+1:]...)
-		var movie Movie 
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("Not Found"))
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			err := json.NewDecoder(r.Body).Decode(&movie)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Invalid request body"))
+				return
+			}
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
 			return
 		}
-		
-		err = json.NewDecoder(r.Body).Decode(&movie)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Invalid request body"))
-			return
-		}
-		
-		 json.NewEncoder(r,Body).Decode(&movie)
-		movie.ID=params["id"]
-		movies=append(movies,movie)
-		json.NewEncoder(w).Encode(movie) 
-
-
 	}
-	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Movie not found"))
 }
+
 func main() {
 	r := mux.NewRouter()
 
-	movies=append(movies,Movie[ID:"1",Isbn:"438227",Title:"Movie One",
-	Director:&Director{Firstname:"David",Lastname:"Amelson"}])
-	movies=append(movies,Movie[ID:"2",Isbn:"425896",Title:"Second Movie",Director:&Director{Firstname:"Alice",Lastname:"Tals"}])
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One",
+		Director: Director{Firstname: "David", Lastname: "Amelson"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "425896", Title: "Second Movie",
+		Director: Director{Firstname: "Alice", Lastname: "Tals"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
-	fmt.Printf("Start Server at port 8000\n")
-	log.Fatal(http.ListenAndServe(":8000" , r))
+
+	fmt.Println("Start Server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
